f6f4acaef0072d7777b03749d1dc308e5f318c6d
Riantix <changjukim24@gmail.com>
2024-06-08 12:16:55 -0400

    Added basic alien movement

--- Assets/Code/AlienScript.cs
+++ Assets/Code/AlienScript.cs
@@ -1,20 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class AlienScript : MonoBehaviour
 {
-    public float speed;
+    public FloatVariable moveCooldownBase;
+    public FloatVariable jumpDistance;
+    private float moveCooldown;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        resetCooldown();
     }
 
-    // Update is called once per frame
-    void Update()
+    // This function is called every fixed framerate frame, if the MonoBehaviour is enabled
+    private void FixedUpdate()
     {
-        
+        moveCooldown -= Time.deltaTime;
+        if (moveCooldown <= 0)
+        {
+            transform.position = transform.position + Vector3.down * jumpDistance.value;
+            resetCooldown();
+        }
     }
+
+    private void resetCooldown()
+    {
+        moveCooldown = moveCooldownBase.value;
+    }
+
+
 }
